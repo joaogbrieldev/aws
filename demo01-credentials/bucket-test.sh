f77e0f574da26f88a7decb56fb3c64d7213e42da
João Figueiredo <joao.ggwp@outlook.com>
2025-01-11 16:17:16 -0300

    feat: demo about create bucket in s3 aws

---
+++ demo01-credentials/bucket-test.sh
@@ -0,0 +1,8 @@
+aws s3api create-bucket --bucket ew-hello-bucket
+
+aws s3 cp hello.txt://ew-hello-bucket
+aws s3 cp s3://ew-hello-bucket/hello.txt h.txt
+
+aws s3 rm s3://ew-hello-bucket --recursive
+aws s3api delete-bucket --bucket ew-hello-bucket
+
